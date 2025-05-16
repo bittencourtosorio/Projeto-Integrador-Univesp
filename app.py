3fdf6aa4ad651ba648af27f0b66c807f9b158637
Rodrigo Bittencourt Osorio <bittencourtosorio@hotmail.com>
2025-05-16 16:38:01 -0300

    Criacao do DB

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from flask import Flask, render_template
+import sqlite3
+from flask_sqlalchemy import SQLAlchemy
+import os, datetime
+
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_file = "sqlite:///{}".format(os.path.join(project_dir, "base_conhecimento_db.db"))
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = database_file
+app.config['SECRET_KEY'] = 'YOUR SECRET KEY HERE'
+db = SQLAlchemy(app)
+
+class Maquinas(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    maquina = db.Column(db.String(20), nullable=False)
 
 @app.route("/")
 
